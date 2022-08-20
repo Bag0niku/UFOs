b9bb4f4b41547d25bf3b84dd322c65fd90a107b9
Bag0niku <45112126+Bag0niku@users.noreply.github.com>
2022-08-19 17:02:00 -0700

    about to test app.js in the webpage

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,7 @@
-function createTable(data) {
+// import the data.js file
+const tableData = data;
+
+function buildTable(data) {
     tbody.html("");  // clear the data from the table for the new freshly filtered results
     
     // iterate through the data building the table
@@ -6,9 +9,30 @@ function createTable(data) {
         let row = tbody.append("tr");
         Object.values(dataRow).forEach((val) => {
             let cell = row.append(td);
-            cell.text(val);
+            cell.text(val);   // each value in the row is a cell in the table row
 
         });
     });
 
-;}
+}
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    
+    // check for filter parameters
+    if (date) {
+        filteredData.filter(row => row.datetime === date);
+    }
+
+    // build the table from the filtered Data
+    buildTable(filteredData);
+
+}
+
+
+// event code attached to the filter button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// initial load of the data on the webpage
+buildTable(tableData);
